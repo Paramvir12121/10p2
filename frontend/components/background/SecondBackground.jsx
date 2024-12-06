dab17be520cbe038857d9f9b31f21910e8fb8cef
Paramvir Singh <paramvir12121@gmail.com>
2024-12-06 04:30:38 +0000

    animate working for second background

--- frontend/components/background/SecondBackground.jsx
+++ frontend/components/background/SecondBackground.jsx
@@ -12,7 +12,10 @@ export default function SecondBackground() {
     const renderer = new THREE.WebGLRenderer();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.position.z = 5;
+    
+
+
+    
 
     if (mountRef.current) {
       mountRef.current.appendChild(renderer.domElement);
@@ -24,17 +27,32 @@ export default function SecondBackground() {
       renderer.setSize(window.innerWidth, window.innerHeight);
     };
 
-    window.addEventListener('resize', handleResize);
+    // window.addEventListener('resize', handleResize);
 
     const axesHelper = new THREE.AxesHelper(5);
     scene.add(axesHelper);
 
-    renderer.render(scene, camera);
-
+    camera.position.set(2,2,10)
+    
 
     
+
+    const BoxGeometry = new THREE.BoxGeometry(1,1,1);
+    const BoxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    const box = new THREE.Mesh(BoxGeometry, BoxMaterial);
+    scene.add(box);
+
+    function animate(){
+        requestAnimationFrame(animate);
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+    
+    animate();
+    
     return () => {
-      window.removeEventListener('resize', handleResize);
+    //   window.removeEventListener('resize', handleResize);
       if (mountRef.current) {
         mountRef.current.removeChild(renderer.domElement);
       }
